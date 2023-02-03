fb702f910ca05f1eca337224339e1b69ad134cbf
PREETI2096 <preetirg96@gmail.com>
2023-02-03 09:54:14 +0530

    Added : gambler program

---
+++ src/com/bridgelabz/gamblinggame/GamblingMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.gamblinggame;
+
+public class GamblingMain {
+	public static void main(String[] args) {
+		int amount = 100;
+		final int BETTING_AMOUNT = 1;
+
+		System.out.println(" Welcome to the Gambler simulator");
+
+		System.out.println(" Gambler starts off with " + amount + " every day");
+		System.out.println(" They place a bet of " + BETTING_AMOUNT);
+	}
+}
